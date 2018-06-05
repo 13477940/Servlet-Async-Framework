22592146f6d5e742618cb5a72d2501d186ecaf2c
TomLi <g13477940@gmail.com>
2018-06-05 18:55:08 +0800

    Update AES_256_GCM.java

--- testasync/WEB-INF/src/framework.encrypt/AES_256_GCM.java
+++ testasync/WEB-INF/src/framework.encrypt/AES_256_GCM.java
@@ -10,15 +10,14 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 /**
- * praseodym/AESGCMUpdateAAD2.java
  * from https://gist.github.com/praseodym/f2499b3e14d872fe5b4a
- * AAD itself is not required（是否有 AAD 並不影響安全性）
  * https://crypto.stackexchange.com/questions/35727/does-aad-make-gcm-encryption-more-secure
  * https://blog.csdn.net/Vieri_32/article/details/48345023
  */
 public class AES_256_GCM {
 
     private SecretKey key = null;
+    private byte[] iv = null;
 
     private Cipher cipher = null;
     private GCMParameterSpec spec = null;
@@ -26,13 +25,26 @@ public class AES_256_GCM {
     private final int GCM_NONCE_LENGTH = 12; // in bytes
     private final int GCM_TAG_LENGTH = 16; // in bytes
 
-    public AES_256_GCM(SecretKey key) {
+    public AES_256_GCM(SecretKey key, byte[] iv) {
         try {
             this.key = key;
-            this.cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE");
+            if(null == key) {
+                try {
+                    SecureRandom random = SecureRandom.getInstanceStrong();
+                    KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+                    keyGen.init(256, random); // key size
+                    this.key = keyGen.generateKey();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            this.iv = iv;
+            if(null == iv) {
+                this.iv = getRandomString(GCM_NONCE_LENGTH).getBytes(StandardCharsets.UTF_8);
+            }
             {
-                byte[] nonce = new byte[GCM_NONCE_LENGTH];
-                this.spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
+                this.cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE");
+                this.spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, this.iv);
             }
         } catch(Exception e) {
             e.printStackTrace();
@@ -43,14 +55,36 @@ public class AES_256_GCM {
         return Base64.getEncoder().withoutPadding().encodeToString(this.key.getEncoded());
     }
 
+    public void setKey(CharSequence charSequence) {
+        if(null == charSequence) {
+            this.key = null;
+            return;
+        }
+        byte[] tmp = Base64.getDecoder().decode(charSequence.toString());
+        this.key = new SecretKeySpec(tmp, 0, tmp.length, "AES");
+    }
+
+    public String getIV() {
+        return Base64.getEncoder().withoutPadding().encodeToString(this.iv);
+    }
+
+    public void setIV(CharSequence charSequence) {
+        if(null == charSequence) {
+            this.iv = null;
+            return;
+        }
+        this.iv = Base64.getDecoder().decode(charSequence.toString());
+    }
+
+    public void updateIV() {
+        this.iv = getRandomString(GCM_NONCE_LENGTH).getBytes(StandardCharsets.UTF_8);
+    }
+
     public String encrypt(CharSequence charSequence) {
         byte[] cipherText = null;
         try {
+            this.spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, this.iv);
             this.cipher.init(Cipher.ENCRYPT_MODE, this.key, this.spec);
-            { // 如果有 updateAAD 則加解密都要啟用
-                byte[] tag = new byte[GCM_TAG_LENGTH];
-                cipher.updateAAD(tag);
-            }
             cipherText = this.cipher.doFinal(charSequence.toString().getBytes(StandardCharsets.UTF_8));
         } catch(Exception e) {
             e.printStackTrace();
@@ -64,11 +98,8 @@ public class AES_256_GCM {
         byte[] encryptBytes = Base64.getDecoder().decode(charSequence.toString());
         byte[] cipherText = null;
         try {
+            this.spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, this.iv);
             this.cipher.init(Cipher.DECRYPT_MODE, this.key, this.spec);
-            { // 如果有 updateAAD 則加解密都要啟用
-                byte[] tag = new byte[GCM_TAG_LENGTH];
-                cipher.updateAAD(tag);
-            }
             cipherText = this.cipher.doFinal(encryptBytes);
         } catch(Exception e) {
             e.printStackTrace();
@@ -77,22 +108,22 @@ public class AES_256_GCM {
         return new String(cipherText, StandardCharsets.UTF_8);
     }
 
+    private String getRandomString(int ranStrSize) {
+        final String ranStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sbd = new StringBuilder();
+        java.util.Random random = new java.util.Random();
+        for(int i = 0; i < ranStrSize; i++) {
+            int iRan = random.nextInt(ranStr.length()-1);
+            char t = ranStr.charAt(iRan);
+            sbd.append(t);
+        }
+        return sbd.toString();
+    }
+
     public static class Builder {
 
-        private final int AES_KEY_SIZE = 256; // key size
         private SecretKey key = null;
-
-        public AES_256_GCM.Builder newKey() {
-            try {
-                SecureRandom random = SecureRandom.getInstanceStrong();
-                KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-                keyGen.init(AES_KEY_SIZE, random);
-                this.key = keyGen.generateKey();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            return this;
-        }
+        private byte[] iv = null;
 
         public AES_256_GCM.Builder setKey(CharSequence charSequence) {
             if(null == charSequence) return this;
@@ -101,8 +132,14 @@ public class AES_256_GCM {
             return this;
         }
 
+        public AES_256_GCM.Builder setIV(CharSequence charSequence) {
+            if(null == charSequence) return this;
+            this.iv = Base64.getDecoder().decode(charSequence.toString());
+            return this;
+        }
+
         public AES_256_GCM build() {
-            return new AES_256_GCM(key);
+            return new AES_256_GCM(this.key, this.iv);
         }
 
     }
