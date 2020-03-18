ee5ccba9ef6ead81193cf4486f659ea63bdaa65f
tomli <g13477940@gmail.com>
2020-03-18 12:17:37 +0800

    change javascript example
    - use web worker
    - use axios replace jquery ajax
    - add web socket example

--- testasync/js/module/index.js
+++ testasync/js/module/index.js
@@ -1,106 +1,38 @@
+"use strict"
 var testasync = {};
 (function() {
     testasync["readyFn"] = function() {
+		// click upload
         $("#upok").on("click", function() {
-            var myfile = $("#upfile")[0]["files"][0];
-            testasync.postData();
-            testasync.postFormData(myfile);
+			var targetFile = $("#upfile")[0]["files"][0];
+			(function(){
+				var worker = new Worker("/testasync/js/worker/web_worker.js");
+				// set listener
+				worker.addEventListener("message", workerMsgFn, false);
+				function workerMsgFn(e) {
+					console.log(e.data);
+					worker.terminate();
+				}
+				// send init msg
+				(function(){
+					var reqObj = {
+						act: "get",
+						url: "/testasync/index",
+						data: [
+							{ key: "a", value: "100" },
+							{ key: "b", value: "ABC" },
+							{ key: "c", value: "國字測試" }
+						]
+					};
+					if(null != targetFile) {
+						// if need upload file, change to form_data mode
+						reqObj["act"] = "form_data";
+						reqObj["data"].push({ key: "myfile", value: targetFile });
+					}
+					worker.postMessage( reqObj );
+				})();
+			})();
         });
-    };
-    testasync["postData"] = function() {
-        var ajax = $.ajax({
-            async: true,
-            type: "post",
-            url: "/testasync/index",
-            beforeSend: function() {}
-        });
-        ajax.done(doneFn);
-        ajax.fail(failFn);
-
-        function doneFn(respd) {
-            console.log(respd);
-        }
-
-        function failFn() {}
-    };
-    testasync["postFormData"] = function(myfile) {
-        var fData = new FormData();
-
-        if (null == myfile) {
-            // fData.append("page", "page");
-            // fData.append("api_type", "member_login");
-            // fData.append("ts", "1495423569106");
-            // fData.append("token", "7c4ffb9a4b227fd13e1b6bc574572862");
-            // fData.append("password", "TestPassword");
-            // fData.append("unicode_test", "中文字測試");
-
-            (function() {
-                var ajax = $.ajax({
-                    async: true,
-                    type: "post",
-                    url: "/testasync/",
-                    data: fData,
-                    processData: false,
-                    contentType: false,
-                    beforeSend: function() {}
-                });
-                ajax.done(doneFn);
-                ajax.fail(failFn);
-
-                function doneFn(respd) {
-                    console.log(respd);
-                }
-
-                function failFn() {}
-            }());
-        } else {
-            // fData.append("page", "page");
-            // fData.append("api_type", "member_login");
-            // fData.append("ts", "1495423569106");
-            // fData.append("token", "7c4ffb9a4b227fd13e1b6bc574572862");
-            fData.append("password", "TestPassword");
-            fData.append("unicode_test", "中文字測試");
-            fData.append("myfile", myfile);
-
-            // 具有上傳進度值的 jQuery AJAX 實作
-            // jQuery 文件上传进度提示：https://segmentfault.com/a/1190000008791342
-            (function() {
-                var ajax = $.ajax({
-                    async: true,
-                    type: "post",
-                    url: "/testasync/index",
-                    data: fData,
-                    processData: false,
-                    contentType: false,
-                    xhr: function() {
-                        var myXhr = $.ajaxSettings.xhr();
-                        if (myXhr.upload) {
-                            myXhr.upload.addEventListener('progress', function(e) {
-                                if (e.lengthComputable) {
-                                    var percent = Math.floor(e.loaded / e.total * 100);
-                                    if (percent <= 100) {
-                                        console.log(percent);
-                                    }
-                                    if (percent >= 100) {
-                                        console.log("upload done.");
-                                    }
-                                }
-                            }, false);
-                        }
-                        return myXhr;
-                    },
-                    beforeSend: function() {}
-                });
-                ajax.done(doneFn);
-                ajax.fail(failFn);
-
-                function doneFn(respd) {
-                    console.log(respd);
-                }
-
-                function failFn() {}
-            }());
-        }
     };
 }());
 $(testasync["readyFn"]);
---
+++ testasync/js/worker/web_worker.js
@@ -0,0 +1,180 @@
+/*
+https://caniuse.com/#feat=webworkers
+https://github.com/axios/axios
+*/
+
+"use strict"
+self.importScripts("/testasync/js/axios/axios.min.js");
+self.addEventListener("message", workerReady, false);
+var workerFn = {};
+
+// WebWorker 可以開始接收 message 時
+function workerReady(e) {
+	var reqObj = e.data;
+	switch(reqObj["act"]) {
+		case "get": { workerFn["get"](reqObj); } break;
+		case "post": { workerFn["post"](reqObj); } break;
+		case "form_data": { workerFn["form_data"](reqObj); } break;
+		case "web_socket": { workerFn["web_socket"]() } break;
+	}
+}
+
+(function(){
+	workerFn["get"] = function(reqObj) {
+		if(null != reqObj["data"] && false == Array.isArray(reqObj["data"])) {
+			console.error("data 參數必須使用 array 型態");
+			return;
+		}
+		var data = {};
+		(function(){
+			var arr = reqObj["data"];
+			for(var index in arr) {
+				var obj = arr[index];
+				var key = obj["key"];
+				var value = obj["value"];
+				data[key] = value;
+			}
+		})();
+		var axiosObj = {
+			method: "get",
+			url: reqObj["url"],
+			params: data
+		};
+		axios(axiosObj).then(function (response) {
+			console.log(response);
+			var respObj = {
+				status: response.status,
+				statusText: response.statusText,
+				data: response.data,
+				headers: response.headers
+				// config: response.config
+				// request: response.request
+			};
+			self.postMessage(respObj);
+		}).catch(function (error) {
+			console.log(error);
+		});
+	};
+})();
+
+(function(){
+	workerFn["post"] = function(reqObj) {
+		if(null != reqObj["data"] && false == Array.isArray(reqObj["data"])) {
+			console.error("data 參數必須使用 array 型態");
+			return;
+		}
+		var data = {};
+		(function(){
+			var arr = reqObj["data"];
+			for(var index in arr) {
+				var obj = arr[index];
+				var key = obj["key"];
+				var value = obj["value"];
+				data[key] = value;
+			}
+		})();
+		var axiosObj = {
+			method: "post",
+			url: reqObj["url"],
+			params: data
+		};
+		axios(axiosObj).then(function (response) {
+			console.log(response);
+			var respObj = {
+				status: response.status,
+				statusText: response.statusText,
+				data: response.data,
+				headers: response.headers
+				// config: response.config
+				// request: response.request
+			};
+			self.postMessage(respObj);
+		}).catch(function (error) {
+			console.log(error);
+		});
+	};
+})();
+
+(function(){
+	workerFn["form_data"] = function(reqObj) {
+		if(null != reqObj["data"] && false == Array.isArray(reqObj["data"])) {
+			console.error("data 參數必須使用 array 型態");
+			return;
+		}
+		var formData = new FormData();
+		(function(){
+			if(null != reqObj["data"]) {
+				var arr = reqObj["data"];
+				for(var index in arr) {
+					var obj = arr[index];
+					formData.append(obj["key"], obj["value"]);
+				}
+			}
+		})();
+		var config = {
+			// maxContentLength: 20000000,
+			onUploadProgress: function(progressEvent) {
+				var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+				var percentStr = String(percentCompleted);
+				console.log("uploadPercent:"+percentStr+"%");
+			}
+		};
+		// 要注意請使用 axios.post()
+		axios.post(reqObj["url"], formData, config).then(function (response) {
+			console.log(response);
+			var respObj = {
+				status: response.status,
+				statusText: response.statusText,
+				data: response.data,
+				headers: response.headers
+				// config: response.config
+				// request: response.request
+			};
+			self.postMessage(respObj);
+		}).catch(function (error) {
+			console.log(error);
+		});
+	};
+})();
+
+// WebSocket Client Example
+(function(){
+	var socketList = [];
+	workerFn["web_socket"] = function() {
+		(function(){
+			function getRootUri() {
+				var ws_protocal = "ws://";
+				var ws_port = ":80";
+				if(location.protocol.indexOf("https") > -1) {
+					ws_protocal = "wss://";
+					ws_port = ":443";
+				}
+				return ws_protocal + location.host + ws_port;
+			}
+			var web_socket = new WebSocket(ws_uri);
+			socketList.push(web_socket);
+			web_socket.onopen = function(evt) {
+				socketReady();
+			};
+			web_socket.onmessage = function(evt) {
+				var eventObj = JSON.parse(evt.data);
+				console.log(eventObj);
+			};
+			web_socket.onerror = function(evt) {
+				reconnectFn();
+			};
+			web_socket.onclose = function(evt) {
+				reconnectFn();
+			};
+			function socketReady() {
+				var sendMsgObj = { "content": "test" };
+				web_socket.send(JSON.stringify(sendMsgObj));
+			}
+			function reconnectFn() {
+				var opt = socketList.shift();
+				opt.close();
+				connectWebSocket();
+			}
+		})();
+	};
+})();
